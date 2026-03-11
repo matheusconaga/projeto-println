60d16d016d0cfb5aadfe115b0aeda7fc09b2313f
matheusconaga <matheusphillip170@gmail.com>
2026-03-11 20:38:36 -0300

    feat(router): implementar sistema centralizado de rotas

---
+++ lib/core/routes/app_routes.dart
@@ -0,0 +1,8 @@
+class AppRoutes{
+
+  static const String splashPage = "/";
+  static const String login = "/login";
+  static const String register = "/register";
+  static const String home = "/home";
+
+}
