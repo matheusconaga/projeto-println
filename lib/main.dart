4e184f0120316acb204c3cbb16f4cd9add7a589a
matheusconaga <matheusphillip170@gmail.com>
2026-03-11 20:41:51 -0300

    chore(projeto): atualizar dependências e configurações do projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:println/views/login/login_page.dart';
+import 'package:println/core/routes/app_routes.dart';
+import 'package:println/core/routes/router_generator.dart';
+import 'package:println/core/theme/app_theme.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main(){
+
+void main() async{
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
   runApp(
       const MyApp()
   );
@@ -14,8 +23,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "PrintLn",
-      home: LoginPage(),
       debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.system,
+      initialRoute: AppRoutes.splashPage,
+      onGenerateRoute: RouteGenerator.generateRoute,
 
     );
   }
